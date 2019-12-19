2f81581471e05aeaa90e1d69cfd341aa66bdc119
Cinara Menezes <cinara.daliana@gmail.com>
2019-12-19 19:30:52 -0300

    adicionar desafio panels

---
+++ challengeday5/index.js
@@ -0,0 +1,12 @@
+const painels = [
+	...document.querySelectorAll('.painel'),
+];
+
+const toggleOpen = e => e.target.classList.toggle('open');
+const toggleOpenActive = e =>
+
+		e.propertyName.includes('flex') ? e.target.classList.toggle('open-active') :
+		null;
+
+panels.map(panel => panel.addEventListener('click', toggleOpen));
+panels.map(panel => panel.addEventListener('transitionend', toggleOpenActive));
